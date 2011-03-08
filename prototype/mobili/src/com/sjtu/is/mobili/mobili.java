7d3ccfd51729e9fcc4bece564f0b5a1681089d3c
xmxjq01@gmail.com <xmxjq01@gmail.com@6b7991a3-62a4-c79a-7a8b-93ea619ff00a>
2011-03-08 14:37:15 +0000

    Prototype test ver02.

--- prototype/mobili/src/com/sjtu/is/mobili/mobili.java
+++ prototype/mobili/src/com/sjtu/is/mobili/mobili.java
@@ -2,10 +2,12 @@ package com.sjtu.is.mobili;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.*;
 
 public class mobili extends Activity {
     WebView test;
+    
 	
 	/** Called when the activity is first created. */
     @Override
@@ -24,5 +26,13 @@ public class mobili extends Activity {
         test.getSettings().setPluginsEnabled(true);
         test.getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
         test.loadUrl("http://www.bilibili.us/play.swf?vid=47658897");
+    }
+    
+    public void openPage(View theButton) {
+        test.loadUrl("http://www.bilibili.us/play.swf?vid=47658897");
+    }
+    
+    public void zoomin(View theButton) {
+    	test.invokeZoomPicker();
     }
 }
